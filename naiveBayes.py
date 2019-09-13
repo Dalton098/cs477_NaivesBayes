a5513cba60acba4b21e5d0237e701710726f4f65
Dalton <dalton098@ymail.com>
2019-09-13 17:55:49 -0400

    Parses CSV now

---
+++ naiveBayes.py
@@ -0,0 +1,50 @@
+import collections
+import csv
+
+# Parses the dataset into 3 different 2d defaultdicts
+def parseData(setosa, versicolor, virginica):
+    with open('IRIS.csv') as csvfile:
+
+        setosaCount = 0
+        versiCount = 0
+        virginicaCount = 0
+
+        readCSV = csv.reader(csvfile, delimiter=',')
+        for row in readCSV:
+            if(row[4] == 'Iris-setosa'):
+                setosa[setosaCount][0] = row[0]
+                setosa[setosaCount][1] = row[1]
+                setosa[setosaCount][2] = row[2]
+                setosa[setosaCount][3] = row[3]
+                setosaCount = setosaCount + 1
+            elif(row[4] == 'Iris-versicolor'):
+                versicolor[versiCount][0] = row[0]
+                versicolor[versiCount][1] = row[1]
+                versicolor[versiCount][2] = row[2]
+                versicolor[versiCount][3] = row[3]
+                versiCount = versiCount + 1
+            elif(row[4] == "Iris-virginica"):
+                virginica[virginicaCount][0] = row[0]
+                virginica[virginicaCount][1] = row[1]
+                virginica[virginicaCount][2] = row[2]
+                virginica[virginicaCount][3] = row[3]
+                virginicaCount = virginicaCount + 1
+
+# DELETE THIS EVENTUALLY
+def prettyPrint(toPrint):
+    for row in toPrint:
+        print(toPrint[row])
+
+
+# Note: For all tables
+# Column 0: Sepal Length
+# Column 1: Sepal Width
+# Column 2: Petal Length
+# Column 3: Petal Width
+setosaDict = collections.defaultdict(lambda: collections.defaultdict(float))
+versicolorDict = collections.defaultdict(lambda: collections.defaultdict(float))
+virginicaDict = collections.defaultdict(lambda: collections.defaultdict(float))
+
+parseData(setosaDict, versicolorDict, virginicaDict)
+
+
