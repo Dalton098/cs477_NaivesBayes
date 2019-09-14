96f078bc422cd38154daccf87362b666d270b9e8
Dalton <dalton098@ymail.com>
2019-09-13 20:19:55 -0400

    Made some good code changes

--- naiveBayes.py
+++ naiveBayes.py
@@ -16,22 +16,16 @@ def parseData(setosa, versicolor, virginica):
         readCSV = csv.reader(csvfile, delimiter=',')
         for row in readCSV:
             if(row[4] == 'Iris-setosa'):
-                setosa[setosaCount][0] = row[0]
-                setosa[setosaCount][1] = row[1]
-                setosa[setosaCount][2] = row[2]
-                setosa[setosaCount][3] = row[3]
+                for i in range(4):
+                    setosa[setosaCount][i] = row[i]
                 setosaCount += 1
             elif(row[4] == 'Iris-versicolor'):
-                versicolor[versiCount][0] = row[0]
-                versicolor[versiCount][1] = row[1]
-                versicolor[versiCount][2] = row[2]
-                versicolor[versiCount][3] = row[3]
+                for i in range(4):
+                    versicolor[versiCount][i] = row[i]
                 versiCount += 1
             elif(row[4] == "Iris-virginica"):
-                virginica[virginicaCount][0] = row[0]
-                virginica[virginicaCount][1] = row[1]
-                virginica[virginicaCount][2] = row[2]
-                virginica[virginicaCount][3] = row[3]
+                for i in range(4):
+                    virginica[virginicaCount][i] = row[i]
                 virginicaCount += 1
     return [setosaCount, versiCount, virginicaCount, setosaCount + versiCount + virginicaCount, ]
 
@@ -41,7 +35,9 @@ def parseData(setosa, versicolor, virginica):
 # 2: 1/2(mean) -> mean
 # 3: mean -> 3/2(mean)
 # 4: 3/2(mean) -> infinity
-def categorize(dict, i, j, mean, halfMean, threeHalfMean):
+
+
+def categorizeData(dict, i, j, mean, halfMean, threeHalfMean):
     toChange = float(dict[i][j])
     if(0 <= toChange and toChange < halfMean):
         dict[i][j] = 1
@@ -53,8 +49,10 @@ def categorize(dict, i, j, mean, halfMean, threeHalfMean):
         dict[i][j] = 4
 
 # Sets up four categories (1, 2, 3, 4) for each column to make the data discrete
-def makeDiscrete(setosa, versicolor, virginica, counts):
 
+
+def makeDiscrete(setosa, versicolor, virginica, counts):
+    toReturn = []
     # 4 for the 4 columns
     for j in range(4):
         mean = 0.0
@@ -66,16 +64,18 @@ def makeDiscrete(setosa, versicolor, virginica, counts):
             mean += float(virginica[i][j])
 
         mean = mean/counts[3]
+        toReturn.append(mean)
         halfMean = mean/2
         threeHalfMean = (3/2 * mean)
 
         for i in range(counts[0]):
-            categorize(setosa, i, j, mean, halfMean, threeHalfMean)
+            categorizeData(setosa, i, j, mean, halfMean, threeHalfMean)
         for i in range(counts[1]):
-            categorize(versicolor, i, j, mean, halfMean, threeHalfMean)
+            categorizeData(versicolor, i, j, mean, halfMean, threeHalfMean)
         for i in range(counts[2]):
-            categorize(virginica, i, j, mean, halfMean, threeHalfMean)
+            categorizeData(virginica, i, j, mean, halfMean, threeHalfMean)
 
+    return toReturn
 
 
 # DELETE THIS EVENTUALLY
@@ -94,8 +94,5 @@ versicolorDict = collections.defaultdict(
     lambda: collections.defaultdict(float))
 virginicaDict = collections.defaultdict(lambda: collections.defaultdict(float))
 
-
 counts = parseData(setosaDict, versicolorDict, virginicaDict)
-makeDiscrete(setosaDict, versicolorDict, virginicaDict, counts)
-
-
+means = makeDiscrete(setosaDict, versicolorDict, virginicaDict, counts)
